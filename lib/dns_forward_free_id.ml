fd02365b53e4bf0e1e2bf5f3124a19434f703d28
David Scott <dave@recoil.org>
2016-11-15 11:50:58 +0000

    Minimise reuse of transaction ids
    Rather than keeping all ids in one big set, we take them from one
    set and put them back in another. When the first set is completely
    empty then we take all the elements from the second set. This should
    cause us to cycle through all available ids, minimising the chance
    of confusion.
    
    Signed-off-by: David Scott <dave@recoil.org>

--- lib/dns_forward_free_id.ml
+++ lib/dns_forward_free_id.ml
@@ -17,8 +17,11 @@
 
 module IntSet = Set.Make(struct type t = int let compare (a: int) (b: int) = compare a b end)
 
+(* try to minimise id reuse by forcing ourselves to cycle through all the free_ids
+   before re-using them *)
 type t = {
-  mutable free_ids: IntSet.t;
+  mutable free_ids: IntSet.t; (* use these first *)
+  mutable used_ids: IntSet.t; (* when free_ids are exhausted, take these back *)
   free_ids_c: unit Lwt_condition.t;
 }
 
@@ -28,22 +31,30 @@ let make ?(max_id = 512) () =
       | 0 -> acc
       | n -> loop (IntSet.add n acc) (n - 1) in
     loop IntSet.empty max_id in
+  let used_ids = IntSet.empty in
   let free_ids_c = Lwt_condition.create () in
-  { free_ids; free_ids_c }
+  { free_ids; used_ids; free_ids_c }
 
 let rec with_id t f =
   let open Lwt.Infix in
   if t.free_ids = IntSet.empty then begin
-    Lwt_condition.wait t.free_ids_c
-    >>= fun () ->
-    with_id t f
+    if t.used_ids = IntSet.empty then begin
+      Lwt_condition.wait t.free_ids_c
+      >>= fun () ->
+      with_id t f
+    end else begin
+      (* start reusing old ids now *)
+      t.free_ids <- t.used_ids;
+      t.used_ids <- IntSet.empty;
+      with_id t f
+    end
   end else begin
     let free_id = IntSet.min_elt t.free_ids in
     t.free_ids <- IntSet.remove free_id t.free_ids;
     Lwt.finalize
       (fun () -> f free_id)
       (fun () ->
-        t.free_ids <- IntSet.add free_id t.free_ids;
+        t.used_ids <- IntSet.add free_id t.used_ids;
         Lwt_condition.signal t.free_ids_c ();
         Lwt.return_unit
       )
